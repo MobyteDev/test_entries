0ddaa9c80da883f19bc960cc8d4a7ba00b474a53
Innres <Innres11@gmail.com>
2022-04-12 19:55:18 +0300

    simple redux

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,22 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_redux/flutter_redux.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-
-import 'login/logic/database.dart';
+import 'package:redux/redux.dart';
 import 'login/login_page.dart';
 import 'notepad/notepad_page.dart';
 import 'notes/notes_page.dart';
 
-void main() => runApp(
-      const ProviderScope(
+void main() {
+  final store = Store<int>(counterReducer, initialState: 0);
+  runApp(
+    StoreProvider(
+      store: store,
+      child: const ProviderScope(
         child: App(),
       ),
-    );
+    ),
+  );
+}
 
 class App extends StatelessWidget {
   const App({Key? key}) : super(key: key);
--- lib/notes/notes_page.dart
+++ lib/notes/notes_page.dart
@@ -1,22 +1,40 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_redux/flutter_redux.dart';
+import 'package:redux/redux.dart';
 import 'package:test_entries/notes/row_text.dart';
+
+class AddActions {}
+
+int counterReducer(int state, dynamic action) {
+  if (action is AddActions) {
+    return state + 1;
+  }
+
+  return state;
+}
 
 class NotesPage extends StatelessWidget {
   const NotesPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    final Store<int> store = StoreProvider.of<int>(context);
     return Scaffold(
       appBar: AppBar(),
       body: SafeArea(
         child: ListView(
           children: [
-            RowText(
-              priority: 2,
-              text: 'qwewq',
-              pressChange: () {},
-              pressAddPriority: () {},
-              pressDelete: () {},
+            StoreConnector<int, int>(
+              converter: (store) => store.state,
+              builder: (context, state) => RowText(
+                priority: state,
+                text: 'qwewq',
+                pressChange: () {},
+                pressAddPriority: () {
+                  store.dispatch(AddActions());
+                },
+                pressDelete: () {},
+              ),
             ),
             RowText(
               priority: 1,
